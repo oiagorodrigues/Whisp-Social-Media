273bbdc54c46e95bcb147c4f83069c82cda76c66
Iago Rodrigues <yagomello16@gmail.com>
2025-04-28 15:41:41 -0300

    feat(be): add login feature

--- django-api/core/auth/viewsets/register.py
+++ django-api/core/auth/viewsets/register.py
@@ -17,16 +17,12 @@ class RegisterViewSet(ViewSet):
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         refresh = RefreshToken.for_user(user)
-        res = {
-            "refresh": str(refresh),
-            "access": str(refresh.access_token),
-        }
 
         return Response(
             {
                 "user": serializer.data,
-                "refresh": res["refresh"],
-                "token": res["access"],
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
             },
             status=status.HTTP_201_CREATED,
         )
--- django-api/core/routers.py
+++ django-api/core/routers.py
@@ -1,11 +1,12 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
-from core.auth.viewsets import RegisterViewSet
+from core.auth.viewsets import RegisterViewSet, LoginViewSet
 
 router = routers.SimpleRouter()
 
 # Auth routes
 router.register(r"auth/register", RegisterViewSet, basename="auth-register")
+router.register(r"auth/login", LoginViewSet, basename="auth-login")
 
 # User routes
 router.register(r"users", UserViewSet, basename="user")
